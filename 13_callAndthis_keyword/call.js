ddf4854f942cc38e8f0926a71b8049d82722a9ef
Harsh_Jha <mharshjha9@gmail.com>
2026-06-02 18:30:16 +0530

    about call keyword

---
+++ 13_callAndthis_keyword/call.js
@@ -0,0 +1,16 @@
+function SetUserName(username){
+    // DB calls
+    this.username = username
+    console.log("called");
+    
+}
+
+function createUser(username,email,password){
+    SetUserName.call(this,username)
+    this.email = email
+    this.password = password
+}
+
+const chai = new createUser("chai","chai@gmail.com","123")
+
+console.log(chai);
